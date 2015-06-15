d8a4af1372cc49050d97bfdbdcbe20a0ced9769c
Eugene Lednev <eugene.lednev@itechcraft.com>
2015-06-15 13:14:11 +0300

    Initial commit

---
+++ CryptoFbChat/MainForm.Designer.cs
@@ -0,0 +1,199 @@
+﻿namespace CryptoFbChat
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.listBoxMembers = new System.Windows.Forms.ListBox();
+            this.textBoxGroupID = new System.Windows.Forms.TextBox();
+            this.label9 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.webBrowser1 = new System.Windows.Forms.WebBrowser();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.buttonStartStreaming = new System.Windows.Forms.Button();
+            this.comboBoxCodecs = new System.Windows.Forms.ComboBox();
+            this.comboBoxInputDevices = new System.Windows.Forms.ComboBox();
+            this.SuspendLayout();
+            // 
+            // listBoxMembers
+            // 
+            this.listBoxMembers.FormattingEnabled = true;
+            this.listBoxMembers.Location = new System.Drawing.Point(363, 135);
+            this.listBoxMembers.Name = "listBoxMembers";
+            this.listBoxMembers.Size = new System.Drawing.Size(120, 95);
+            this.listBoxMembers.TabIndex = 39;
+            this.listBoxMembers.Visible = false;
+            // 
+            // textBoxGroupID
+            // 
+            this.textBoxGroupID.Location = new System.Drawing.Point(367, 54);
+            this.textBoxGroupID.Name = "textBoxGroupID";
+            this.textBoxGroupID.Size = new System.Drawing.Size(108, 20);
+            this.textBoxGroupID.TabIndex = 38;
+            this.textBoxGroupID.Text = "1418644935108471";
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label9.Location = new System.Drawing.Point(382, 16);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(73, 19);
+            this.label9.TabIndex = 37;
+            this.label9.Text = "GroupID";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label7.Location = new System.Drawing.Point(382, 105);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(79, 19);
+            this.label7.TabIndex = 36;
+            this.label7.Text = "Members";
+            this.label7.Visible = false;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label6.Location = new System.Drawing.Point(23, 46);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(74, 19);
+            this.label6.TabIndex = 35;
+            this.label6.Text = "myname";
+            this.label6.Visible = false;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label5.Location = new System.Drawing.Point(5, 16);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(131, 19);
+            this.label5.TabIndex = 34;
+            this.label5.Text = "Facebook User:";
+            this.label5.Visible = false;
+            // 
+            // webBrowser1
+            // 
+            this.webBrowser1.Location = new System.Drawing.Point(-6, 16);
+            this.webBrowser1.MinimumSize = new System.Drawing.Size(20, 20);
+            this.webBrowser1.Name = "webBrowser1";
+            this.webBrowser1.ScrollBarsEnabled = false;
+            this.webBrowser1.Size = new System.Drawing.Size(337, 229);
+            this.webBrowser1.TabIndex = 33;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(-8, 107);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(67, 13);
+            this.label4.TabIndex = 32;
+            this.label4.Text = "Compression";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(-8, 80);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(71, 13);
+            this.label3.TabIndex = 31;
+            this.label3.Text = "Input Device:";
+            // 
+            // buttonStartStreaming
+            // 
+            this.buttonStartStreaming.Location = new System.Drawing.Point(39, 149);
+            this.buttonStartStreaming.Name = "buttonStartStreaming";
+            this.buttonStartStreaming.Size = new System.Drawing.Size(129, 23);
+            this.buttonStartStreaming.TabIndex = 30;
+            this.buttonStartStreaming.Text = "Start Streaming";
+            this.buttonStartStreaming.UseVisualStyleBackColor = true;
+            // 
+            // comboBoxCodecs
+            // 
+            this.comboBoxCodecs.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.comboBoxCodecs.FormattingEnabled = true;
+            this.comboBoxCodecs.Location = new System.Drawing.Point(69, 104);
+            this.comboBoxCodecs.Name = "comboBoxCodecs";
+            this.comboBoxCodecs.Size = new System.Drawing.Size(242, 21);
+            this.comboBoxCodecs.TabIndex = 29;
+            // 
+            // comboBoxInputDevices
+            // 
+            this.comboBoxInputDevices.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.comboBoxInputDevices.FormattingEnabled = true;
+            this.comboBoxInputDevices.Location = new System.Drawing.Point(69, 77);
+            this.comboBoxInputDevices.Name = "comboBoxInputDevices";
+            this.comboBoxInputDevices.Size = new System.Drawing.Size(242, 21);
+            this.comboBoxInputDevices.TabIndex = 28;
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(510, 278);
+            this.Controls.Add(this.listBoxMembers);
+            this.Controls.Add(this.textBoxGroupID);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.webBrowser1);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.buttonStartStreaming);
+            this.Controls.Add(this.comboBoxCodecs);
+            this.Controls.Add(this.comboBoxInputDevices);
+            this.Name = "MainForm";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListBox listBoxMembers;
+        private System.Windows.Forms.TextBox textBoxGroupID;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.WebBrowser webBrowser1;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button buttonStartStreaming;
+        private System.Windows.Forms.ComboBox comboBoxCodecs;
+        private System.Windows.Forms.ComboBox comboBoxInputDevices;
+    }
+}
+
---
+++ CryptoFbChat/MainForm.cs
@@ -0,0 +1,287 @@
+﻿using Facebook;
+using NAudio.Wave;
+using NAudioDemo.NetworkChatDemo;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Runtime.Serialization.Formatters.Binary;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading;
+using System.Windows.Forms;
+
+namespace CryptoFbChat
+{
+    public partial class MainForm : Form
+    {
+        private string myLocalIp = "192.168.1.2";
+        private WaveInEvent waveIn;
+        private IWavePlayer waveOut;
+        private BufferedWaveProvider waveProvider;
+        private UdpClient udpSender;
+        private UdpClient udpListener;
+        private INetworkChatCodec selectedCodec;
+        private volatile bool connected = false;
+        private RijndaelManaged myRijndael = new RijndaelManaged();
+        private Int64 fbAppId = 1404447426539494;
+        string redirectFbPath = "https://apps.facebook.com/nurecryptochat";
+        Dictionary<int, IPEndPoint> threadMappings = new Dictionary<int, IPEndPoint>();
+        RSACryptoServiceProvider myouRSA = new RSACryptoServiceProvider();
+        int inputDeviceNumber;
+
+        public MainForm()
+        {
+            InitializeComponent();
+            PopulateInputDevicesCombo();
+
+            List<INetworkChatCodec> codecs = new List<INetworkChatCodec>();
+            codecs.Add(new ALawChatCodec());
+            codecs.Add(new MuLawChatCodec());
+            PopulateCodecsCombo(codecs);
+            Disposed += OnFormDisposed;
+            startFbLogin();
+            listBoxMembers.Items.Clear();
+        }
+
+        private void startFbLogin()
+        {
+            var fb = new FacebookClient();
+            var query = new Dictionary<string, object>();
+            query["client_id"] = fbAppId;
+            query["redirect_uri"] = redirectFbPath;
+            query["response_type"] = "token";
+            query["display"] = "popup";
+
+            Uri loginUri = fb.GetLoginUrl(query);
+            webBrowser1.Navigate(loginUri);
+        }
+
+        void OnFormDisposed(object sender, EventArgs e)
+        {
+            Disconnect();
+        }
+
+        private void PopulateCodecsCombo(IEnumerable<INetworkChatCodec> codecs)
+        {
+            comboBoxCodecs.Items.Add(new CodecComboItem { Text = "No codec compression", Codec = null });
+
+            var sorted = from codec in codecs
+                         where codec.IsAvailable
+                         orderby codec.BitsPerSecond ascending
+                         select codec;
+
+            foreach (var codec in sorted)
+            {
+                string bitRate = codec.BitsPerSecond == -1 ? "VBR" : String.Format("{0:0.#}kbps", codec.BitsPerSecond / 1000.0);
+                string text = String.Format("{0} ({1})", codec.Name, bitRate);
+                comboBoxCodecs.Items.Add(new CodecComboItem { Text = text, Codec = codec });
+            }
+
+            comboBoxCodecs.SelectedIndex = 0;
+        }
+
+        class CodecComboItem
+        {
+            public string Text { get; set; }
+            public INetworkChatCodec Codec { get; set; }
+            public override string ToString()
+            {
+                return Text;
+            }
+        }
+
+        private void PopulateInputDevicesCombo()
+        {
+            for (int n = 0; n < WaveIn.DeviceCount; n++)
+            {
+                var capabilities = WaveIn.GetCapabilities(n);
+                comboBoxInputDevices.Items.Add(capabilities.ProductName);
+            }
+            if (comboBoxInputDevices.Items.Count > 0)
+            {
+                comboBoxInputDevices.SelectedIndex = 0;
+            }
+        }
+        
+        private void GiveAESKeyAsync()
+        {
+            int port = 7081;
+            if (threadMappings[Thread.CurrentThread.ManagedThreadId].ToString().StartsWith("109"))
+                port = 7082;
+            TcpClient tcpConnection = new TcpClient(new IPEndPoint(IPAddress.Parse(myLocalIp), port));
+            tcpConnection.Connect(threadMappings[Thread.CurrentThread.ManagedThreadId]);
+            var stream = tcpConnection.GetStream();
+
+            BinaryFormatter serializer = new BinaryFormatter();
+            RSAParameters myouParams = (RSAParameters)serializer.Deserialize(stream);
+            myouRSA.ImportParameters(myouParams);
+            byte[] encryptedByRsaAesKey = myouRSA.Encrypt(myRijndael.Key, true);
+            tcpConnection.Client.Send(encryptedByRsaAesKey);
+            tcpConnection.Close();
+            threadMappings.Remove(Thread.CurrentThread.ManagedThreadId);
+        }
+
+        private void ConnectOneMemberAsync()
+        {
+            IPEndPoint currentMemberIp = threadMappings[Thread.CurrentThread.ManagedThreadId];
+            Connect(currentMemberIp, inputDeviceNumber, selectedCodec);
+        }
+
+        private void Connect(IPEndPoint endPoint, int inputDeviceNumber, INetworkChatCodec codec)
+        {
+            waveIn = new WaveInEvent();
+            waveIn.BufferMilliseconds = 50;
+            waveIn.DeviceNumber = inputDeviceNumber;
+            if (codec != null)
+                waveIn.WaveFormat = codec.RecordFormat;
+            else
+                waveIn.WaveFormat = new WaveFormat(8000, 16, 1);
+            waveIn.DataAvailable += waveIn_DataAvailable;
+            waveIn.StartRecording();
+
+            udpSender = new UdpClient();
+            udpListener = new UdpClient();
+
+            udpListener.Client.SetSocketOption(SocketOptionLevel.Socket, SocketOptionName.ReuseAddress, true);
+            udpListener.Client.Bind(new IPEndPoint(IPAddress.Parse(myLocalIp), 7080));
+
+            udpSender.Connect(endPoint);
+
+            waveOut = new WaveOut(WaveCallbackInfo.FunctionCallback());
+            waveProvider = new BufferedWaveProvider(waveIn.WaveFormat);
+            waveOut.Init(waveProvider);
+            waveOut.Play();
+
+            connected = true;
+            var state = new ListenerThreadState { Codec = codec, EndPoint = endPoint };
+            ThreadPool.QueueUserWorkItem(ListenerThread, state);
+        }
+
+        private void Disconnect()
+        {
+            if (connected)
+            {
+                connected = false;
+                waveIn.DataAvailable -= waveIn_DataAvailable;
+                waveIn.StopRecording();
+                waveOut.Stop();
+
+                udpSender.Close();
+                udpListener.Close();
+                waveIn.Dispose();
+                waveOut.Dispose();
+
+                if (selectedCodec != null)
+                    selectedCodec.Dispose();
+            }
+        }
+
+        class ListenerThreadState
+        {
+            public IPEndPoint EndPoint { get; set; }
+            public INetworkChatCodec Codec { get; set; }
+        }
+
+        private void ListenerThread(object state)
+        {
+            var listenerThreadState = (ListenerThreadState)state;
+            var endPoint = listenerThreadState.EndPoint;
+            try
+            {
+                while (connected)
+                {
+                    byte[] b = udpListener.Receive(ref endPoint);
+
+                    string roundtrip = DecryptStringFromBytes(b, myRijndael.Key, myRijndael.IV);
+                    byte[] decrypted = Encoding.Unicode.GetBytes(roundtrip);
+
+                    if (listenerThreadState.Codec != null)
+                    {
+                        byte[] decoded = listenerThreadState.Codec.Decode(decrypted, 0, decrypted.Length);
+                        waveProvider.AddSamples(decoded, 0, decoded.Length);
+                    }
+                    else
+                        waveProvider.AddSamples(decrypted, 0, decrypted.Length);
+                }
+            }
+            catch (SocketException)
+            {
+            }
+        }
+
+        private byte[] EncryptStringToBytes(string plainText, byte[] Key, byte[] IV)
+        {
+            if (plainText == null || plainText.Length <= 0)
+                throw new ArgumentNullException("plainText");
+            if (Key == null || Key.Length <= 0)
+                throw new ArgumentNullException("Key");
+            if (IV == null || IV.Length <= 0)
+                throw new ArgumentNullException("IV");
+            byte[] encrypted;
+
+
+            ICryptoTransform encryptor = myRijndael.CreateEncryptor(myRijndael.Key, myRijndael.IV);
+
+            using (MemoryStream msEncrypt = new MemoryStream())
+            {
+                using (CryptoStream csEncrypt = new CryptoStream(msEncrypt, encryptor, CryptoStreamMode.Write))
+                {
+                    using (StreamWriter swEncrypt = new StreamWriter(csEncrypt))
+                    {
+                        swEncrypt.Write(plainText);
+                    }
+                    encrypted = msEncrypt.ToArray();
+                }
+            }
+
+            return encrypted;
+        }
+
+        private string DecryptStringFromBytes(byte[] cipherText, byte[] Key, byte[] IV)
+        {
+            if (cipherText == null || cipherText.Length <= 0)
+                throw new ArgumentNullException("cipherText");
+            if (Key == null || Key.Length <= 0)
+                throw new ArgumentNullException("Key");
+            if (IV == null || IV.Length <= 0)
+                throw new ArgumentNullException("IV");
+
+            string plaintext = null;
+
+            ICryptoTransform decryptor = myRijndael.CreateDecryptor(myRijndael.Key, myRijndael.IV);
+
+            using (MemoryStream msDecrypt = new MemoryStream(cipherText))
+            {
+                using (CryptoStream csDecrypt = new CryptoStream(msDecrypt, decryptor, CryptoStreamMode.Read))
+                {
+                    using (StreamReader srDecrypt = new StreamReader(csDecrypt))
+                    {
+                        plaintext = srDecrypt.ReadToEnd();
+                    }
+                }
+            }
+
+            return plaintext;
+        }
+
+        void waveIn_DataAvailable(object sender, WaveInEventArgs e)
+        {
+            byte[] encoded;
+            if (selectedCodec == null)
+            {
+                encoded = new byte[e.BytesRecorded];
+                Array.Copy(e.Buffer, encoded, e.BytesRecorded);
+            }
+            else
+                encoded = selectedCodec.Encode(e.Buffer, 0, e.BytesRecorded);
+
+            string s = Encoding.Unicode.GetString(encoded);
+            byte[] encrypted = EncryptStringToBytes(s, myRijndael.Key, myRijndael.IV);
+
+            udpSender.Send(encrypted, encrypted.Count());
+        }
+    }
+}
